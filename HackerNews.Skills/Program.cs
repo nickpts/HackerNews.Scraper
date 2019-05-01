31c9390471b7f41528ec1033925ea47d33299d5e
Nick <nicholas.patsaris@gmail.com>
2019-05-01 19:33:58 +0100

    Add project files.

---
+++ HackerNews.Skills/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerNews.Skills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
