a5d83ec4e09a94830cc4d5e1c45dc7c792834d78
Nick <nicholas.patsaris@gmail.com>
2019-05-12 23:40:46 +0100

    initial commit

--- HackerNews.Skills/Program.cs
+++ HackerNews.Skills/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using HackerNews.Reader;
+using Microsoft.Extensions.Configuration;
 
 namespace HackerNews.Skills
 {
@@ -7,6 +14,26 @@ namespace HackerNews.Skills
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var reader = new PostReader(100, CommentLevel.FirstLevel);
+			var post = reader.GetById(new int[] { 19797594 }, new CancellationToken()).First();
+
+			var config = new ConfigurationBuilder().AddJsonFile("settings.json").Build();
+
+			var values = config["skills"].Split(',');
+
+			var filteredPosts = GetPostBySkill(post, values).ToList();
         }
+
+		public static IEnumerable<Post> GetPostBySkill(Post post, string[] values)
+		{
+			foreach (var comment in post.Comments)
+			{
+				if (comment.Text != null && comment.Text.ToLower().Contains(values.First()))
+				{
+					yield return comment;
+				}
+			}
+		}
     }
 }
